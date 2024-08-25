28511dbf4e6b7ae223d870f81d93a77c4b5d20ab
jisu-kim <kss2460@naver.com>
2024-08-25 10:32:29 +0900

    feat: 사이드바 구조 중간 작업

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,12 @@ const config = {
       navbar: {
         title: 'FE Tech Talk',
         items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',

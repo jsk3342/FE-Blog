d073ee3314abb72920bc58af12d74a00f145c8d6
jisu-kim <kss2460@naver.com>
2024-08-31 09:18:12 +0900

    feat: disqus 추가

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import DocItem from '@theme-original/DocItem';
+import { DiscussionEmbed } from 'disqus-react';
 
 export default function DocItemWrapper(props) {
   return (
     <>
       <DocItem {...props} />
+      <DocItemContentWithComments {...props} />
     </>
+  );
+}
+
+function DocItemContentWithComments(props) {
+  const { metadata } = props.content;
+  const { permalink, title } = metadata;
+
+  const disqusShortname = 'fe-tech-2'; 
+  const disqusConfig = {
+    url: window.location.href,
+    identifier: permalink,
+    title: title,
+  };
+
+  return (
+    <div id="disqus_thread" style={{ marginTop: '2rem' }}>
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+    </div>
   );
 }

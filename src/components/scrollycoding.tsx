ee18fb89b7824fa6ac2662f8a200064749701d10
jisu-kim <kss2460@naver.com>
2024-10-22 16:32:21 +0900

    feat: css 추가

--- src/components/scrollycoding.tsx
+++ src/components/scrollycoding.tsx
@@ -1,36 +1,27 @@
-import { z } from "zod"
-import {
-  Selection,
-  Selectable,
-  SelectionProvider,
-} from "codehike/utils/selection"
-import { Block, HighlightedCodeBlock, parseProps } from "codehike/blocks"
-import { HighlightedCode, Pre } from "codehike/code"
+import { z } from 'zod';
+import { Selection, Selectable, SelectionProvider } from 'codehike/utils/selection';
+import { Block, HighlightedCodeBlock, parseProps } from 'codehike/blocks';
+import { HighlightedCode, Pre } from 'codehike/code';
 
-import { tokenTransitions } from "./annotations/token-transitions"
-import { wordWrap } from "./annotations/word-wrap"
-import styles from "./scrollycoding.module.css"
+import { tokenTransitions } from './annotations/token-transitions';
+import { wordWrap } from './annotations/word-wrap';
+import styles from './scrollycoding.module.css';
 
 const Schema = Block.extend({
   steps: z.array(
     Block.extend({
       code: HighlightedCodeBlock,
-    })
+    }),
   ),
-})
+});
 
 export function Scrollycoding(props: unknown) {
-  const { steps } = parseProps(props, Schema)
+  const { steps } = parseProps(props, Schema);
   return (
     <SelectionProvider className={styles.container}>
       <div className={styles.stepsContainer}>
         {steps.map((step, i) => (
-          <Selectable
-            key={i}
-            index={i}
-            selectOn={["click", "scroll"]}
-            className={styles.step}
-          >
+          <Selectable key={i} index={i} selectOn={['click', 'scroll']} className={styles.step}>
             <h2 className={styles.stepTitle}>{step.title}</h2>
             <div>{step.children}</div>
           </Selectable>
@@ -42,7 +33,7 @@ export function Scrollycoding(props: unknown) {
         ))}
       />
     </SelectionProvider>
-  )
+  );
 }
 
 function CodeSticker({ codeblock }: { codeblock: HighlightedCode }) {
@@ -52,9 +43,9 @@ function CodeSticker({ codeblock }: { codeblock: HighlightedCode }) {
         <Pre
           code={codeblock}
           handlers={[tokenTransitions, wordWrap]}
-          style={{ minHeight: "40rem", backgroundColor: "transparent" }}
+          style={{ minHeight: '40rem', backgroundColor: 'transparent' }}
         />
       </div>
     </div>
-  )
+  );
 }
